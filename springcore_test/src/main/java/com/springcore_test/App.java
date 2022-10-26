f1d93eae16968bb74affb1a5588870af5ef73200
Md. Talal Wasim <md.talalwasim@gmail.com>
2022-10-26 11:32:04 +0600

    Three ways of Inject values in Setter Injection
    Three ways of Inject values in Setter Injection

--- springcore_test/src/main/java/com/springcore_test/App.java
+++ springcore_test/src/main/java/com/springcore_test/App.java
@@ -13,9 +13,13 @@ public class App
     {
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Student std1 = (Student)context.getBean("student1");
+        Student std1 = (Student) context.getBean("student1");
+        Student std2 =(Student) context.getBean("student2");
+        Student std3 =(Student) context.getBean("student3");
         
         System.out.println(std1);
+        System.out.println(std2);
+        System.out.println(std3);
         
         
     }
